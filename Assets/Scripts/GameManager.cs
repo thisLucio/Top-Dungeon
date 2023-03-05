f400847a7d0acf1c467f811c163e0c78daa28cae
thisLucio <luxusnietzsche@gmail.com>
2023-03-05 03:19:02 -0300

    Add Save and Load State

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    private void Awake()
+    {
+        if(GameManager.instance != null){
+            Destroy(gameObject);
+            return;
+        }
+        // PlayerPrefs.DeleteAll();
+
+        instance = this;
+        SceneManager.sceneLoaded += LoadState;
+        DontDestroyOnLoad(gameObject);
+     
+    }
+    
+    //Resources
+    public List<Sprite> playerSprites;
+    public List<Sprite> weaponSprites;
+    public List<int> weaponPrices;
+    public List<int> xpTable;
+
+    //References
+    public Player player; 
+    
+	//Logic
+	public int pesos;
+	public int experience;
+
+    // Save State
+    /*
+        INT preferedSkin
+        INT pesos
+        INT experience
+        INT weaponLevel
+    */
+	public void SaveState(){
+        string s = "";
+        s += "0" + "|";
+        s += pesos.ToString() + "|";
+        s += experience.ToString() + "|";
+        s += "0";
+        PlayerPrefs.SetString("SaveState", s);
+        
+	}
+
+	public void LoadState(Scene s, LoadSceneMode mode){
+        
+        if(!PlayerPrefs.HasKey("SaveState"))
+            return;
+
+        string[] data = PlayerPrefs.GetString("SaveState").Split('|');
+        
+        //ChangeSkin
+        pesos = int.Parse(data[1]);
+        experience = int.Parse(data[2]);
+        //ChangeWeaponLevel
+        Debug.Log("LoadState");
+
+	}
+    
+}
